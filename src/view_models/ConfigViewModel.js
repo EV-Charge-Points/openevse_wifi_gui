fa75edfa5d25234484ef2b0c1677867d205d3fda
Jeremy Poulter <jeremy@bigjungle.net>
2019-12-20 22:00:47 +0000

    Removed some references to OpenEVSE

--- src/view_models/ConfigViewModel.js
+++ src/view_models/ConfigViewModel.js
@@ -30,7 +30,7 @@ function ConfigViewModel(baseEndpoint) {
     "ohmkey": "",
     "www_username": "",
     "www_password": "",
-    "hostname": "openevse",
+    "hostname": "evcp",
     "firmware": "-",
     "protocol": "-",
     "espflash": 0,
